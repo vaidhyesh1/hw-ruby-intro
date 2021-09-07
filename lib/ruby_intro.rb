eaed88bb0888b49fca7afbf0a46bb90458133100
EC2 Default User <ec2-user@ip-172-31-11-182.us-east-2.compute.internal>
2021-09-07 17:49:50 +0000

    Part 1 Q1 and Q2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,10 +4,40 @@
 
 def sum arr
   # YOUR CODE HERE
+  sumNum = 0
+  if arr.length() != 0
+    arr.each {|a| sumNum += a}
+  end
+  return sumNum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length() == 0
+    return 0
+  elsif arr.length() == 1
+    return arr[0]
+  end
+  if arr[0] > arr[1]
+    firstMax = arr[0]
+    secondMax = arr[1]
+  else
+    firstMax = arr[1]
+    secondMax = arr[0]
+  end
+  arr.each_with_index {|val,index|
+  if index == 0 or index == 1
+  then next
+  end
+  if val>firstMax 
+  then 
+  firstMax = val
+  elsif val <= firstMax and val>=secondMax 
+  then secondMax = val
+  end
+  }
+  return firstMax + secondMax
+    
 end
 
 def sum_to_n? arr, n
